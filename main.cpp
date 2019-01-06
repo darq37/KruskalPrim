890d9a9820dd484484ad67bbf2275dc89cb05d86
Dominik Zmudziak <dzmudziak@gmail.com>
2019-01-06 22:28:24 +0100

    explanation

--- main.cpp
+++ main.cpp
@@ -10,9 +10,15 @@ using namespace std;
 
 int znajdzNajblizszyWierzcholek(int *odleglosc, bool *odwiedzone, int iloscWierzcholkow);
 
-void wypiszWynikPrima(int **graph, int *parent, int n);
+void wypiszWYnikPrima(int **graph, int *parent, int n);
 
 int *Prim(int **graph, int iloscWierzcholkow);
+
+int *Kruskal(int **graph, int n, int k);
+
+void uniteTrees(int z, int b, int *parents);
+
+int pickTree(int n, int *parents);
 
 int **wygenerujSpojnyGraf(int n, int k);
 
@@ -45,32 +51,40 @@ int main() {
             cout << "Czytam dane z pliku: " << endl;
             plik.open(R"(C:\Users\wesiv\Desktop\KRUSKAL\dane.txt)");
             getline(plik, line);
-            n = stoi(line);
+            n = stoi(line); //string to int(line) -> n
+            /*bpobiera pierwszą linijke i zapisuje ją jako liczbe wierzcholków*/
 
 
-            int **macierzsasiedztwa = stworzMacierzWypelnionaZerami(n);
+            int **macierzsasiedztwa = stworzMacierzWypelnionaZerami(n); // tworze macierz z zerami i przypisuje ją do wskaźnika na macierz
 
-            while (getline(plik, line)) {
-                cout << line << '\n';
-                istringstream s(line);
-                string v1, v2, waga;
+            while (getline(plik, line)) { // póki mam jeszcze linie w pliku
+                cout << line << '\n'; // wypisuje wszystkie stringi na ekran
+                istringstream s(line); // klasa input stream do operacji na stringach, tworze obiekt s
+                string v1, v2, waga; // deklaruje 3 stringi
                 getline(s, v1, ';');
                 getline(s, v2, ';');
-                getline(s, waga);
+                getline(s, waga); // przypisuje do każdej zmiennej wartosc stringa z lini pliku ( wartosc przypisana do obiektu s), wartosci oddzielone średnikami
+                /*składnia: getline (istream&  is, string& str, char delimeter);*/
                 wstawSymetrycznieDoMacierzy(macierzsasiedztwa, stoi(v1), stoi(v2), stoi(waga));
+                /*uzywam funkcji z poprzedniego algortmu, która wstawia mi odpowiednie wartosci (v1 i v2 oraz wage) go macierzy w symetryczny sposób
+                 * jednoczesnie uzywam funkcji string to int, parsując stringi do wartosci typu int*/
             }
-            wypiszMacierz(n, macierzsasiedztwa);
-            int m = 0;
+            wypiszMacierz(n, macierzsasiedztwa); // wypisuje na ekran macierz
+            int m = 0; // zmienna pomocnicza w wypisaniu krawędzi
             for (int i = 0; i < n; ++i) {
                 for (int j = 0; j < n; ++j) {
 
-                    if (macierzsasiedztwa[i][j] != 0) m++;
+                    if (macierzsasiedztwa[i][j] != 0) m++;/* iterujac przez macierz, jezeli nie ma 0 w środku tablicy, zwiększam m*/
                 }
             }
-            k = m / 2;
+            k = m / 2; // poniewaz macierz jest symetryczna, wszystkie niezerowe miejsca w tablicy to krawędzie, a liczba krawędzi jest dwukrotnie mniejsza
+            // krawędzie potrzbne do Kruskala
             cout << "Ilość krawędzi: " << k;
-            Prim(macierzsasiedztwa, n);
-            wypiszWynikPrima(macierzsasiedztwa, Prim(macierzsasiedztwa, n), n);
+            cout << "\nWynik wywolania algorytmu Prima :" <<endl;
+            Prim(macierzsasiedztwa, n); // uzywam algorytmu prima
+            wypiszWYnikPrima(macierzsasiedztwa, Prim(macierzsasiedztwa, n), n); // wypisuje wynik Prima
+            cout << "Wynik wywolania algorytmu Kruskala :" <<endl;
+            Kruskal(macierzsasiedztwa, n, k); // uzywam algorytmu Kruskala, który wypisuje wynik w swojej implementacji
             plik.close();
 
             break;
@@ -90,12 +104,13 @@ int main() {
             double zapelnienie;
             cout << "Podaj zapelnenie grafu" << endl;
             cin >> zapelnienie;
-            cout << "Zapełnienie grafu wynosi " << zapelnienie << "%." << endl;
+            cout << "Zapełnienie grafu wynosi " << zapelnienie << "%." << endl; /* kopia z poprzedniego algorytmu*/
             for (int i = zakresMinimum; i < zakresMaksimum; ++i) {
                 int maksymalnaIloscKrawedzi = i * (i - 1) / 2;
                 k = static_cast<int>(maksymalnaIloscKrawedzi * zapelnienie / 100);
                 cout << '\n' << "Graf [" << i << "] ma teraz " << k << " krawędzi" << endl;
                 double czas = 0;
+                double czas2 = 0;/* dwie zmienne do liczenia czasu prima i kruskala*/
                 for (int j = 0; j < liczbaWykonan; j++) {
                     int **macierzSasiedztwa = wygenerujSpojnyGraf(i, k);
                     /*wypiszMacierz(i, macierzSasiedztwa);*/
@@ -103,8 +118,15 @@ int main() {
                     Prim(macierzSasiedztwa, i);
                     auto done = chrono::steady_clock::now();
                     czas += chrono::duration<double, nano>(done - started).count();
+                    auto started2 = chrono::steady_clock::now();
+                    Kruskal(macierzSasiedztwa, i, k); // wypisywanie danych nie powinno miec tutaj miejsca, trzeba  by to rozdzielic, jak w przypadku Prima
+                    auto done2 = chrono::steady_clock::now();
+                    czas2 += chrono::duration<double, nano>(done2 - started2).count();
                 }
-                cout << '\n' << "Sredni czas: " << czas / liczbaWykonan << " nano sekund" << '\n';
+                cout << '\n' << "Sredni czas dla Prima: " << czas / liczbaWykonan << " nano sekund" << '\n';
+                cout << '\n' << "Sredni czas dla Kruskala: " << czas2 / liczbaWykonan << " nano sekund" << '\n';
+
+
             }
             break;
         }
@@ -116,49 +138,51 @@ int main() {
     return 0;
 }
 
-void wypiszWynikPrima(int **graph, int *parent, int n) {
+void wypiszWYnikPrima(int **graph, int *parent, int n) {
     cout << '\n' << "Rodzic: \t" << "Cel: \t" << "Waga: " << endl;
     for (int i = 1; i < n; i++) {
-        printf("%d\t\t-> \t%d \t\t%d \n", parent[i], i, graph[i][parent[i]]);
+        printf("%d\t\t-> \t%d \t\t%d \n", parent[i], i, graph[i][parent[i]]); // wypisuje elementy tablicy rodziców, indeks danego elementu (cel) oraz
+        /*wstawiam element rodzica jako indeks do macierzy sasiedztwa, odczytujac przy tym jego wagę*/
     }
 }
 
 int *Prim(int **graph, int iloscWierzcholkow) {
     //
     int *parent = new int[iloscWierzcholkow];
-    int *odleglosc = new int[iloscWierzcholkow];
-    bool *odwiedzone = new bool[iloscWierzcholkow];
+    int *odleglosc = new int[iloscWierzcholkow];/*tworze wskaźniki na tablice intów z odleglsociami rodzicami*/
+    bool *odwiedzone = new bool[iloscWierzcholkow];/* oraz wskaźnik na tablice booli, sprawdzajacą czy wierzcholek byl juz odwieczony*/
     for (int i = 0; i < iloscWierzcholkow; i++) {
         odleglosc[i] = INT_MAX;
-        odwiedzone[i] = false;
+        odwiedzone[i] = false;/*ustawiam wszystkie elementy tablic odleglosci na INT_MAX i tablicy potwierdzającej na false*/
     }
     odleglosc[0] = 0;
     parent[0] = -1;
     for (int count = 0; count < iloscWierzcholkow - 1; count++) { //idać przez wszystkie wierzchołki (w pierwszym już byłem)
-        int najblizszyWiercholek = znajdzNajblizszyWierzcholek(odleglosc, odwiedzone, iloscWierzcholkow); /*przyjmuje index
+        int najblizszyWierzcholek = znajdzNajblizszyWierzcholek(odleglosc, odwiedzone, iloscWierzcholkow); /*przyjmuje index
  * jeszcze nie odwiedzonego wierzcholka o najmniejszej wadze*/
-        odwiedzone[najblizszyWiercholek] = true; /*Zapisuje wierzcholek o tym indexie jako odwiedzony*/
+        odwiedzone[najblizszyWierzcholek] = true; /*Zapisuje index wierzcholka o najmniejszej wadze jako odwiedzony*/
         for (int j = 0; j < iloscWierzcholkow; j++) { /*Idąc przez wszystkie wierzchołki*/
-            if (graph[najblizszyWiercholek][j]) { /*Jeżeli jestem w najblizszym wierzchołku*/
+            if (graph[najblizszyWierzcholek][j]) { /*będąc w wierzcholku oznacznym jako najblizszy*/
                 if (!odwiedzone[j]) { /*i nie był on odwiedzony*/
-                    if (graph[najblizszyWiercholek][j] < odleglosc[j]) { /*Jeżeli wierzcholek jest najblizszy*/
-                        parent[j] = najblizszyWiercholek; /*zapisuje jego index jako rodzica*/
-                        odleglosc[j] = graph[najblizszyWiercholek][j]; /*nadpisuje odleglosc*/
+                    if (graph[najblizszyWierzcholek][j] < odleglosc[j]) { /*Jeżeli waga tego wierzcholka jest nizsza od odleglosci do nastepnego wierzcholka */
+                        parent[j] = najblizszyWierzcholek; /*zapisuje jego index jako rodzica*/
+                        odleglosc[j] = graph[najblizszyWierzcholek][j]; /*nadpisuje odleglosc*/
                     }
                 }
             }
         }
     }
-    return parent;
+    return parent;/*zwracam rodzica w celu wypisania wyniku wywolania algorytmu na ekran*/
 }
 
 int znajdzNajblizszyWierzcholek(int *odleglosc, bool *odwiedzone, int iloscWierzcholkow) {
-    int min = INT_MAX;
-    int min_index;
-    for (int i = 0; i < iloscWierzcholkow; i++)
-        if (odwiedzone[i] == false && odleglosc[i] < min) //jeżeli wierzchołek nie był odwiedzony, aktualizuje tymczasowa
-            // odległość, dążąć do najmniejszej
-            min = odleglosc[i], min_index = i;
+    int min = INT_MAX; // ustawiam zmienna pomocnicza min na int amx
+    int min_index; // deklaruje zmienna pomocnicza oznaczającą index wierzcholka u aktualnie najmniejszej wadze
+    for (int i = 0; i < iloscWierzcholkow; i++) // idac przez wszystkie wierzcholki
+        if (odwiedzone[i] == false && odleglosc[i] < min) //jeżeli wierzchołek nie był jeszcze odwiedzony, aktualizuje tymczasowa
+            // odległość, dążąc do najmniejszej
+            min = odleglosc[i], min_index = i; /*w pierwszym kroku ustawiam min na 0, oraz index na 0
+ * w nastepnych iteracjach, szukam najmniejszej odleglosci*/
     return min_index; //zwraca index nie odwiedzonego jeszcze wierzcholka o najmniejszej odległości
 }
 
@@ -234,21 +258,46 @@ vector<pair<int, int>> znajdzMiejscaNaKrawedzie(int n, int *const *macierzSasied
     return puste;
 }
 
-int *Kruskal(int **graph,int n,  int k){
-    int *parents = new int [n];
-    int iloscOdiwedzonychKrawedzi = 0;
+int *Kruskal(int **graph, int n, int k) {
+    int *parent = new int[n];/*tworze wskaznik na tablice zawierającą wierzcholki z ktorych wyszeldem*/
+    int iloscOdwiedzonychKrawedzi = 0;
     int odleglosci = 0;
     for (int i = 0; i < n; i++) {
-        parents[i] = i; // każdy wierzchołek jest teraz rodzicem
+        parent[i] = i; // ustawiam indeks kazdego wierzcholka do tablicy rodzicow
     }
-    while(iloscOdiwedzonychKrawedzi< n-1){
-
-
-        iloscOdiwedzonychKrawedzi++;
+    cout << '\n' << "Rodzic: \t" << "Cel: \t" << "Waga: " << endl;
+    while (iloscOdwiedzonychKrawedzi < n - 1) {/*dopoki nie skoncza mi sie krawędzie*/
+        int minimum = INT_MAX;/*ustawiam minimum na nieskoncz*/
+        int a1 = -1;
+        int a2 = -1;/*ustawiam dwie zmienne pomocnicze na -1, bye nie kolidowac z wierzcholkami*/
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {/*iteruje po macierzy sasiedztwa*/
+                if (graph[i][j] != 0 && pickTree(i, parent) != pickTree(j, parent) && graph[i][j] < minimum) {/*jezeli dla 2 wierzcholkow istnieje krawedz ORAZ
+ * jezeli ta krawedz nie łaczy wierzcholkow, z ktorych wychodzi ta krawędz, ORAZ jezeli waga krawedzi jest mniejsza od minimum*/
+                    minimum = graph[i][j];/*ustawiam wage tej krawędzi na minium, a wiec dodaje ją do MST*/
+                    a1 = i;/*oraz ustalam zmienne pomocnicze na odpowiednie wiercholki, tworzące krawędź*/
+                    a2 = j;
+                }
+            }
+        }
+        uniteTrees(a1, a2, parent);/*lacze powstale ewentualne drzewa, na podstawie rodzica krawędzi */
+        printf("%d\t\t-> \t%d \t\t%d \n", a1, a2, minimum); //wypisuje wynik, to powinno isc od innej funkcji
+        odleglosci += minimum;/*nadpisuje odleglosci*/
+        iloscOdwiedzonychKrawedzi++;/*zwiekszam ilosc odwiedzonych krawędzi*/
     }
-
-
-
     return parent;
 }
+
+void uniteTrees(int z, int b, int *parents) {/*funkcja do laczenia powstaluch mniejszych drzew spinajacych*/
+    int a1 = pickTree(z, parents);/* ustawiam zmienna a1 jako  jedno drzewo na podstawie pierwszej krawędzie*/
+    int a2 = pickTree(b, parents);/*ustawiam drugie drzewo*/
+    parents[a1] = a2;/* lacze oba drzewa, na podstawie rodzica*/
+}
+
+int pickTree(int n, int *parents) {/*funkcja do wybierania drzewa, przyjmuje krawedz oraz tablice rodzicow*/
+    while (parents[n] != n)/*póki wartosc elementu z tablicy rodzicow d nie jest rowna indexowi tego elementu*/
+        n = parents[n];/*ustawiam wartosc n-tego elementu tablicy pod zienna n*/
+    return n;/*oraz zwracam ta zmienna*/
+}
+
 
